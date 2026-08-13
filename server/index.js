81c3932cf76a0dec2e1d0f5377841eb78996c5c1
aswinkarna-max <aswinkarna17@gmail.com>
2026-08-14 02:31:22 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://thriftly-flame.vercel.app/login'
+    'https://thriftly-flame.vercel.app'
   ],
   credentials: true
 }))
