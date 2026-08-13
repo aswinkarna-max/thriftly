72412a667dac9e15bca9a8f9be27cf39f2c80a8f
aswinkarna-max <aswinkarna17@gmail.com>
2026-08-14 01:44:45 +0530

    Update generateToken.js for adding cors request communication btw vercel and render

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -8,8 +8,8 @@ const generateToken = (res, userId) => {
   // Store token in HTTP-only cookie (more secure than localStorage)
   res.cookie('jwt', token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    secure: process.env.NODE_ENV === 'production', // must be true when sameSite is 'none'
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
   })
 }
